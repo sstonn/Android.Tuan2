663c551c9671ce803d606455625202359f3cf5fe
Hood <43521744+toantoan222@users.noreply.github.com>
2019-03-26 00:57:05 +0700

    first commit

---
+++ app/src/main/java/com/java/btvn/btvntuan2/Bai2.kt
@@ -0,0 +1,12 @@
+package com.java.btvn.btvntuan2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Bai2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bai2)
+    }
+}
---
+++ app/src/main/java/com/java/btvn/btvntuan2/Bai3.kt
@@ -0,0 +1,12 @@
+package com.java.btvn.btvntuan2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Bai3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bai3)
+    }
+}
